faedc1b1613d08c9ad753264b1e4cb0138335e3a
Tomno <tomnoviv@gmail.com>
2023-05-19 12:40:38 +0300

    Complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    
+    def walk(self):
+        print("The person is walking.")
+    def talk(self):
+        print("Hello World!")
+    
+guido = Person()
+guido.walk()
+guido.talk()   
